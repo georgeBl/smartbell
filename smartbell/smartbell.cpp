f95b3defc678d156524e2990aab811e09ef7ae38
georgebl <blanarugeorge96@gmail.com>
2022-04-19 11:50:55 +0100

    added song to c++ on button

--- smartbell/smartbell.cpp
+++ smartbell/smartbell.cpp
@@ -2,19 +2,20 @@
 //
 
 #include <iostream>
+#include <windows.h>
+#include <wingdi.h>
+#pragma comment(lib, "winmm.lib")
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{	
+	
+	int a = 10;
+	for (;;) {
+		// if 1 button was pressed
+		if (GetAsyncKeyState((int)65)) {
+			printf("Key was pressed\n");
+			PlaySound(TEXT("vericu.wav"), NULL, SND_FILENAME | SND_ASYNC);// - the correct code
+			Sleep(1000);
+		}
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
