03e61ee0277567d1851801ece51b07451bdbfc23
georgebl <blanarugeorge96@gmail.com>
2022-04-22 18:13:51 +0100

    play oon any key

--- smartbell/smartbell.cpp
+++ smartbell/smartbell.cpp
@@ -1,21 +1,50 @@
 // smartbell.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 #include <windows.h>
 #include <wingdi.h>
+#include <errhandlingapi.h>
 #pragma comment(lib, "winmm.lib")
 
+#include <vector>
 int main()
-{	
-	
-	int a = 10;
-	for (;;) {
-		// if 1 button was pressed
-		if (GetAsyncKeyState((int)65)) {
-			printf("Key was pressed\n");
-			PlaySound(TEXT("vericu.wav"), NULL, SND_FILENAME | SND_ASYNC);// - the correct code
-			Sleep(1000);
-		}
+{
+	// Assign vector
+	std::vector<int> v;
+
+	// fill the array with 10 five times
+	v.assign(5, 10);
+
+	for (auto a : v) {
+		a -= 1;
 	}
+	v.erase(v.begin()+2);
+
+
+	for (auto a : v) {
+		std::cout << a << "\n";
+	}
+
+/*
+	while(1) {
+		for (int i = 3; i < 256; i++) {
+			if ((GetAsyncKeyState(i)) ) {
+				printf("Caracter %d\n", i);
+
+				
+				break;
+			}
+		}
+		Sleep(1000);
+	}
+	*/
 }
+	
+
+		// if 1 button was pressed
+		//if (GetKeyboardState(*keys)) {
+		//	printf("Key was pressed\n");
+		//	PlaySound(TEXT("mywavsound.wav"), NULL, SND_FILENAME | SND_ASYNC);// - the correct code
+		//	Sleep(1000);
+
+
